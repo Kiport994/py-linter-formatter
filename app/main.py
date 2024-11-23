33d8cbf1ae149fb1245ede80977209461e1589fd
Yakushev Oleksandr <oleksandr.yakushev@student.tuke.sk>
2024-11-23 11:52:31 +0100

    i did it

--- app/main.py
+++ app/main.py
@@ -1,13 +1,54 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return \
+        {
+            "line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
+        }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return \
+        {
+            "errors":
+                [
+                    {
+                        "line": key["line_number"],
+                        "column": key["column_number"],
+                        "message": key["text"],
+                        "name": key["code"],
+                        "source": "flake8"
+                    }
+                    for key in errors
+                ],
+            "path": file_path,
+            "status": "failed" if errors != [] else "passed"
+
+        }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return \
+        [
+            format_single_linter_file(
+                "./test_source_code_2.py",
+                linter_report["./test_source_code_2.py"]
+            ),
+
+            format_single_linter_file(
+                "./source_code_2.py",
+                linter_report["./source_code_2.py"]
+            ),
+
+            format_single_linter_file(
+                "./source_code_1.py",
+                linter_report["./source_code_1.py"]
+            ),
+
+            format_single_linter_file(
+                "./test_source_code_1.py",
+                linter_report["./test_source_code_1.py"]
+            )
+        ]
